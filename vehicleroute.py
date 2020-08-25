60b10689c8a46ac37352824c4ef4acf0d8256148
Yannick Moise Ilunga <54799716+yannickmoiseilunga@users.noreply.github.com>
2020-08-25 09:01:18 +0200

    Update vehicleroute.py

--- vehicleroute.py
+++ vehicleroute.py
@@ -7,7 +7,15 @@ from os.path import splitext
 from io import BytesIO
 import datetime
 from datetime import timedelta
+from PIL import Image
 
+import pandas
+from pandas import DataFrame
+import numpy
+from dateutil.parser import parse
+
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
 
 class Map:
     def __init__(self, points):
@@ -16,7 +24,8 @@ class Map:
         :param points: a list of tuples contains latitude, longitude and time of a route
         """
         self._points = points
-        self.google_coordinates = ",\n".join(["{{lat: {lat}, lng: {lng}, tm: {tm}}}".format(lat=x, lng=y, tm=z) for x, y, z, *rest in self._points])
+        self.v = []
+        self.google_coordinates = ",\n".join(["{{lat: {lat}, lng: {lng}, alt: {alt}, dist: {dist}, tm: {tm}}}".format(lat=x, lng=y, alt=a, dist=d, tm=z) for x, y, a, d, z, *rest in self._points])
 
     @property
     def zoom(self):
@@ -31,11 +40,9 @@ class Map:
         max_zoom = 21   # maximum zoom level based on Google Map API
         world_dimension = {'height': 256, 'width': 256}     # min map size for entire world
 
-        latitudes  = [lat for lat, lon, tm, *rest in self._points]
-        longitudes = [lon for lat, lon, tm, *rest in self._points]
-        triptime   = [tm for lat, lon, tm, *rest in self._points]
-
-
+        latitudes  = [lat for lat, lng, *rest in self._points]
+        longitudes = [lng for lat, lng, *rest in self._points]
+    
         # calculate longitude span between east and west
         delta = max(longitudes) - min(longitudes)
         if delta < 0:
@@ -58,11 +65,11 @@ class Map:
         Calculate the center of the current map object
         :return: (center_lat, center_lng) latitude, longitude represents the center of the map object
         """
-        center_lat = (max((x[0] for x in self._points)) + min((x[0] for x in self._points))) / 2
-        center_lng = (max((x[1] for x in self._points)) + min((x[1] for x in self._points))) / 2
-        trip_tme = (max((x[2] for x in self._points)) + min((x[2] for x in self._points))) / 2
-
-        return center_lat, center_lng, trip_tme
+        latitudes  = [lat for lat, lng, *rest in self._points]
+        longitudes = [lng for lat, lng, *rest in self._points]
+        center_lat = (max((x for x in latitudes))  + min((x for x in latitudes)))  / 2
+        center_lng = (max((x for x in longitudes)) + min((x for x in longitudes))) / 2
+        return center_lat, center_lng
 
     @property
     def altitude_svg(self):
@@ -70,24 +77,43 @@ class Map:
         Create an svg data object using matplotlib for altitude chart that can be injected into html template
         :return: altitude_svg; svg data for altitude chart
         """
-        timevar   = [tm for *rest, tm in self._points]
+        timevar   = [tm for lat, lng, alt, tm, *rest in self._points]
         altitudes = [alt for lat, lng, alt, *rest in self._points]
         distances = [dist for *rest, dist in self._points]
-
-        plt.figure(figsize=(15, 2.5))
+        plt.figure(figsize=(15, 5.5))
         plt.ylabel('Time')
         plt.xlabel('Distance')
-        plt.tight_layout()
-        #plt.plot(distances, timevar altitudes)
-        plt.plot(distances, timevar)
+
+        fig, (ax1, ax2) =  plt.subplots(2)
+        fig.suptitle('Altitude vs. Distance / Time vs. Distance')
+        ax1.plot(distances, altitudes)
+        ax2.plot(distances, timevar)
         svg_file = BytesIO()
         plt.savefig(svg_file, format='svg')     # save the file to io.BytesIO
         svg_file.seek(0)
 
-        #altitude_svg = svg_file.getvalue().decode()   # retreive the saved data
         altitude_svg = svg_file.getvalue().decode()   # retreive the saved data
         altitude_svg = '<svg' + altitude_svg.split('<svg')[1]   # strip the xml header
         return altitude_svg
+
+    
+    @property
+    def altitude_svg1(self):
+        speedvar     = [Speedv for *rest, Speedv in self._points]
+        timevar        = [Time for *rest, Time in self._points]
+        distancesvar = [dist for *rest, dist in self._points]
+
+        plt.figure(figsize=(15, 2.5))
+        plt.ylabel('Speed')
+        plt.xlabel('Distance')
+        plt.plot(timevar, speedvar)
+        svg_file1 = BytesIO()
+        plt.savefig(svg_file1, format='svg')     # save the file to io.BytesIO
+        svg_file1.seek(1)
+
+        altitude_svg1 = svg_file1.getvalue().decode()   # retreive the saved data
+        altitude_svg1 = '<svg' + altitude_svg1.split('<svg')[1]   # strip the xml header
+        return altitude_svg1
 
     @staticmethod
     def _lat_rad(lat):
@@ -97,13 +123,6 @@ class Map:
         sinus = math.sin(math.radians(lat + math.pi / 180))
         rad_2 = math.log((1 + sinus) / (1 - sinus)) / 2
         return max(min(rad_2, math.pi), -math.pi) / 2
-
-    def _vel_road(tm, dist):
-        """
-        Helper function for calculating velocity
-        """
-        return dist / tm
-
 
 class Route:
     """
@@ -120,8 +139,9 @@ class Route:
         self.title = splitext(route_file)[0]
         self.trackpoints = []
         self.trip_velocity = []
-        #self.vlc_tm = []
-        #var diff 
+        self.vlc_tm = 0
+        self.t = [] 
+        self.v = [] 
 
         try:
             dom = minidom.parse(route_file)
@@ -134,31 +154,27 @@ class Route:
     def _parse_trackpoints(self, trkpts):
         for trackpoint in trkpts:
             tm_node = trackpoint.getElementsByTagName("Time")[0]
-            tm = self._to_local(tm_node.firstChild.data)
+            tm_node = self._to_local(tm_node.firstChild.data)
+            datetimeFormat = '%Y-%m-%d %H:%M:%S'
+            tm = datetime.datetime.strptime(tm_node, datetimeFormat)
 
             lat_node = trackpoint.getElementsByTagName("LatitudeDegrees")[0]
             lat = float(lat_node.firstChild.data)
 
             lng_node = trackpoint.getElementsByTagName("LongitudeDegrees")[0]
             lng = float(lng_node.firstChild.data)
-
-            
+           
             # In the case of Endomondo, AltitudeMeters is not always available
             try:
                 alt_node = trackpoint.getElementsByTagName("AltitudeMeters")[0]
                 alt = float(alt_node.firstChild.data)
             except IndexError:
-                alt = 0.0
+                alt = 0
 
             dist_node = trackpoint.getElementsByTagName("DistanceMeters")[0]
             dist = float(dist_node.firstChild.data)
-            #trip_velocity = float(dist) / float(tm)
-            #vlc_tm = self._to_velocity(self)
-
-            #self.trackpoints.append((lat, lng, alt, tm, dist, trip_velocity))
+           
             self.trackpoints.append((lat, lng, alt, tm, dist))
-        
-
 
     @staticmethod
     def _to_local(utc_datetime):
@@ -168,44 +184,5 @@ class Route:
         utc = dt.datetime.strptime(utc_datetime, '%Y-%m-%dT%H:%M:%SZ')
         offset = dt.datetime.utcnow() - dt.datetime.now()
         return (utc - offset).strftime("%Y-%m-%d %H:%M:%S")
-"""
-    @property
-    def _to_velocity(self):
-        
-        #trip_elaptme  = self.trackpoints[2][-1] - self.trackpoints[2][0] 
-        #trip_elaptme  = self.trackpoints - self.trackpoints
-        #trip_dist     = self.trackpoints[-1][-1] - self.trackpoints[-1][0]  
-        #trip_velocity = trip_dist / float(trip_elaptme)
-        trip_velocity = self.trackpoints 
-        #trip_tme = (max((x[3] for x in self.trackpoints)) - min((x[3] for x in self.trackpoints))).strftime("%Y-%m-%d %H:%M:%S") 
-        #trip_elaptme = (max((x[3] for x in self.trackpoints)) - min((x[3] for x in self.trackpoints))).strftime("%S") 
-        #trip_elaptme = tm[tm.length -1] - tm[0]  
-        #trip_dist = (max((x[4] for x in self.trackpoints)) - min((x[4] for x in self.trackpoints))) 
-        #trip_velocity = trip_dist / float(trip_elaptme)
-
-        #utc = dt.datetime.strptime(utc_datetime, '%Y-%m-%dT%H:%M:%SZ')
-        #offset = dt.datetime.utcnow() - dt.datetime.now()
 
 
-        trip_elaptme  = self.trackpoints[-2][-1] - self.trackpoints[-2][0] 
-        trip_dist     = self.trackpoints[-1][-1] - self.trackpoints[-1][0]  
-        trip_velocity = trip_dist / float(trip_elaptme)
-        #vlc_tm = self._to_velocity(self, trip_dist, trip_elaptme)
-
-        datetimeFormat = '%Y-%m-%d %H:%M:%S.%f'
-        #date1 = trackpoints.tm[0] 
-        #date2 = trackpoints.tm[-1]
-
-        date1 = self.trackpoints[-2][0]  
-        date2 = self.trackpoints[-2][-1]
-         
-        #diff = datetime.datetime.strptime(date1, datetimeFormat) - datetime.datetime.strptime(date2, datetimeFormat)
- 
-        diff = date1 - date2
- 
-        return trip_velocity
-"""
-
-"""
-        #Helper function to calculate velocity
-        """
